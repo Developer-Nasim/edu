2af5a0fe7e19bc8604ba085809a5d540d1f9bfaf
Developer-Nasim <ajnasim72@gmail.com>
2022-08-09 01:39:27 +0600

    added login pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,6 +114,27 @@
     })
   }
 
+  function FormValidation() {
+    if (document.querySelectorAll('.needs-validation').length > 0) {
+          // Fetch all the forms we want to apply custom Bootstrap validation styles to
+          var forms = document.querySelectorAll('.needs-validation')
+
+          // Loop over them and prevent submission
+          Array.prototype.slice.call(forms)
+              .forEach(function (form) {
+              form.addEventListener('submit', function (event) {
+                  if (!form.checkValidity()) {
+                  event.preventDefault()
+                  event.stopPropagation()
+                  }
+
+                  form.classList.add('was-validated')
+              }, false)
+              })
+    }
+  }
+  FormValidation()
+
 //   Show more or less
     function ShowMoreLess() { 
         if (document.querySelectorAll('.showMoreLessNow').length > 0) {
