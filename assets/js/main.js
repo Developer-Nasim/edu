eadc8dd76e2711046cbaac563e3946f327ab2d8e
Developer-Nasim <ajnasim72@gmail.com>
2022-08-15 00:21:46 +0600

    all fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,19 +48,18 @@
   }
 
 
-//   tag Edit
-
-EditSaveVoorkeur()
-function EditSaveVoorkeur() {
-    if (document.querySelectorAll('.tgEdtBtn').length > 0) { 
-        let EditBtns = document.querySelectorAll('.tgEdtBtn')
-        EditBtns.forEach(btn => {
-            btn.addEventListener('click', () => {
-                btn.parentElement.classList.toggle('offMode') 
-            })
-        });
+//   tag Edit 
+    EditSaveVoorkeur()
+    function EditSaveVoorkeur() {
+        if (document.querySelectorAll('.tgEdtBtn').length > 0) { 
+            let EditBtns = document.querySelectorAll('.tgEdtBtn')
+            EditBtns.forEach(btn => {
+                btn.addEventListener('click', () => {
+                    btn.parentElement.classList.toggle('offMode') 
+                })
+            });
+        }
     }
-}
 
   // show success popup
   SuccessPopup()
@@ -186,6 +185,310 @@ function EditSaveVoorkeur() {
         }
     }
     ShowMoreLess()
+
+
+    // Time Range
+    function TheTimeRange() {
+        
+        $(".slider-range").slider({
+            range: true,
+            min: 0,
+            max: 1440,
+            step: 15,
+            values: [540, 1020],
+            slide: function (e, ui) {
+                var hours1 = Math.floor(ui.values[0] / 60);
+                var minutes1 = ui.values[0] - (hours1 * 60);
+
+                if (hours1.length == 1) hours1 = '0' + hours1;
+                if (minutes1.length == 1) minutes1 = '0' + minutes1;
+                if (minutes1 == 0) minutes1 = '00';
+                if (hours1 >= 24) {
+                    if (hours1 == 24) {
+                        hours1 = hours1;
+                        minutes1 = minutes1;
+                    } else {
+                        hours1 = hours1 - 12;
+                        minutes1 = minutes1;
+                    }
+                } else {
+                    hours1 = hours1;
+                    minutes1 = minutes1;
+                }
+                if (hours1 == 0) {
+                    hours1 = 0;
+                    minutes1 = minutes1;
+                }
+
+
+
+                $('.st').html(hours1 + ':' + minutes1);
+
+                var hours2 = Math.floor(ui.values[1] / 60);
+                var minutes2 = ui.values[1] - (hours2 * 60);
+
+                if (hours2.length == 1) hours2 = '0' + hours2;
+                if (minutes2.length == 1) minutes2 = '0' + minutes2;
+                if (minutes2 == 0) minutes2 = '00';
+                if (hours2 >= 24) {
+                    if (hours2 == 24) {
+                        hours2 = hours2;
+                        minutes2 = minutes2;
+                    } else if (hours2 == 24) {
+                        hours2 = 11;
+                        minutes2 = "59";
+                    } else {
+                        hours2 = hours2 - 12;
+                        minutes2 = minutes2;
+                    }
+                } else {
+                    hours2 = hours2;
+                    minutes2 = minutes2;
+                }
+
+                $('.st1_1').html(hours2 + ':' + minutes2);
+            }
+        });
+        $(".slider-range-2").slider({
+            range: true,
+            min: 0,
+            max: 1440,
+            step: 15,
+            values: [540, 1020],
+            slide: function (e, ui) {
+                var hours1 = Math.floor(ui.values[0] / 60);
+                var minutes1 = ui.values[0] - (hours1 * 60);
+
+                if (hours1.length == 1) hours1 = '0' + hours1;
+                if (minutes1.length == 1) minutes1 = '0' + minutes1;
+                if (minutes1 == 0) minutes1 = '00';
+                if (hours1 >= 24) {
+                    if (hours1 == 24) {
+                        hours1 = hours1;
+                        minutes1 = minutes1;
+                    } else {
+                        hours1 = hours1 - 12;
+                        minutes1 = minutes1;
+                    }
+                } else {
+                    hours1 = hours1;
+                    minutes1 = minutes1;
+                }
+                if (hours1 == 0) {
+                    hours1 = 0;
+                    minutes1 = minutes1;
+                }
+
+
+
+                $('.st2').html(hours1 + ':' + minutes1);
+
+                var hours2 = Math.floor(ui.values[1] / 60);
+                var minutes2 = ui.values[1] - (hours2 * 60);
+
+                if (hours2.length == 1) hours2 = '0' + hours2;
+                if (minutes2.length == 1) minutes2 = '0' + minutes2;
+                if (minutes2 == 0) minutes2 = '00';
+                if (hours2 >= 24) {
+                    if (hours2 == 24) {
+                        hours2 = hours2;
+                        minutes2 = minutes2;
+                    } else if (hours2 == 24) {
+                        hours2 = 11;
+                        minutes2 = "59";
+                    } else {
+                        hours2 = hours2 - 12;
+                        minutes2 = minutes2;
+                    }
+                } else {
+                    hours2 = hours2;
+                    minutes2 = minutes2;
+                }
+
+                $('.st2_2').html(hours2 + ':' + minutes2);
+            }
+        });
+        $(".slider-range-3").slider({
+            range: true,
+            min: 0,
+            max: 1440,
+            step: 15,
+            values: [540, 1020],
+            slide: function (e, ui) {
+                var hours1 = Math.floor(ui.values[0] / 60);
+                var minutes1 = ui.values[0] - (hours1 * 60);
+
+                if (hours1.length == 1) hours1 = '0' + hours1;
+                if (minutes1.length == 1) minutes1 = '0' + minutes1;
+                if (minutes1 == 0) minutes1 = '00';
+                if (hours1 >= 24) {
+                    if (hours1 == 24) {
+                        hours1 = hours1;
+                        minutes1 = minutes1;
+                    } else {
+                        hours1 = hours1 - 12;
+                        minutes1 = minutes1;
+                    }
+                } else {
+                    hours1 = hours1;
+                    minutes1 = minutes1;
+                }
+                if (hours1 == 0) {
+                    hours1 = 0;
+                    minutes1 = minutes1;
+                }
+
+
+
+                $('.st3').html(hours1 + ':' + minutes1);
+
+                var hours2 = Math.floor(ui.values[1] / 60);
+                var minutes2 = ui.values[1] - (hours2 * 60);
+
+                if (hours2.length == 1) hours2 = '0' + hours2;
+                if (minutes2.length == 1) minutes2 = '0' + minutes2;
+                if (minutes2 == 0) minutes2 = '00';
+                if (hours2 >= 24) {
+                    if (hours2 == 24) {
+                        hours2 = hours2;
+                        minutes2 = minutes2;
+                    } else if (hours2 == 24) {
+                        hours2 = 11;
+                        minutes2 = "59";
+                    } else {
+                        hours2 = hours2 - 12;
+                        minutes2 = minutes2;
+                    }
+                } else {
+                    hours2 = hours2;
+                    minutes2 = minutes2;
+                }
+
+                $('.st3_2').html(hours2 + ':' + minutes2);
+            }
+        });
+        $(".slider-range-4").slider({
+            range: true,
+            min: 0,
+            max: 1440,
+            step: 15,
+            values: [540, 1020],
+            slide: function (e, ui) {
+                var hours1 = Math.floor(ui.values[0] / 60);
+                var minutes1 = ui.values[0] - (hours1 * 60);
+
+                if (hours1.length == 1) hours1 = '0' + hours1;
+                if (minutes1.length == 1) minutes1 = '0' + minutes1;
+                if (minutes1 == 0) minutes1 = '00';
+                if (hours1 >= 24) {
+                    if (hours1 == 24) {
+                        hours1 = hours1;
+                        minutes1 = minutes1;
+                    } else {
+                        hours1 = hours1 - 12;
+                        minutes1 = minutes1;
+                    }
+                } else {
+                    hours1 = hours1;
+                    minutes1 = minutes1;
+                }
+                if (hours1 == 0) {
+                    hours1 = 0;
+                    minutes1 = minutes1;
+                }
+
+
+
+                $('.st4').html(hours1 + ':' + minutes1);
+
+                var hours2 = Math.floor(ui.values[1] / 60);
+                var minutes2 = ui.values[1] - (hours2 * 60);
+
+                if (hours2.length == 1) hours2 = '0' + hours2;
+                if (minutes2.length == 1) minutes2 = '0' + minutes2;
+                if (minutes2 == 0) minutes2 = '00';
+                if (hours2 >= 24) {
+                    if (hours2 == 24) {
+                        hours2 = hours2;
+                        minutes2 = minutes2;
+                    } else if (hours2 == 24) {
+                        hours2 = 11;
+                        minutes2 = "59";
+                    } else {
+                        hours2 = hours2 - 12;
+                        minutes2 = minutes2;
+                    }
+                } else {
+                    hours2 = hours2;
+                    minutes2 = minutes2;
+                }
+
+                $('.st4_2').html(hours2 + ':' + minutes2);
+            }
+        });
+        $(".slider-range-5").slider({
+            range: true,
+            min: 0,
+            max: 1440,
+            step: 15,
+            values: [540, 1020],
+            slide: function (e, ui) {
+                var hours1 = Math.floor(ui.values[0] / 60);
+                var minutes1 = ui.values[0] - (hours1 * 60);
+
+                if (hours1.length == 1) hours1 = '0' + hours1;
+                if (minutes1.length == 1) minutes1 = '0' + minutes1;
+                if (minutes1 == 0) minutes1 = '00';
+                if (hours1 >= 24) {
+                    if (hours1 == 24) {
+                        hours1 = hours1;
+                        minutes1 = minutes1;
+                    } else {
+                        hours1 = hours1 - 12;
+                        minutes1 = minutes1;
+                    }
+                } else {
+                    hours1 = hours1;
+                    minutes1 = minutes1;
+                }
+                if (hours1 == 0) {
+                    hours1 = 0;
+                    minutes1 = minutes1;
+                }
+
+
+
+                $('.st5').html(hours1 + ':' + minutes1);
+
+                var hours2 = Math.floor(ui.values[1] / 60);
+                var minutes2 = ui.values[1] - (hours2 * 60);
+
+                if (hours2.length == 1) hours2 = '0' + hours2;
+                if (minutes2.length == 1) minutes2 = '0' + minutes2;
+                if (minutes2 == 0) minutes2 = '00';
+                if (hours2 >= 24) {
+                    if (hours2 == 24) {
+                        hours2 = hours2;
+                        minutes2 = minutes2;
+                    } else if (hours2 == 24) {
+                        hours2 = 11;
+                        minutes2 = "59";
+                    } else {
+                        hours2 = hours2 - 12;
+                        minutes2 = minutes2;
+                    }
+                } else {
+                    hours2 = hours2;
+                    minutes2 = minutes2;
+                }
+
+                $('.st5_2').html(hours2 + ':' + minutes2);
+            }
+        });
+    }
+    TheTimeRange()
+
+
 
   // StepForm 
   let closeNow = document.querySelectorAll('.ResetPopup');
